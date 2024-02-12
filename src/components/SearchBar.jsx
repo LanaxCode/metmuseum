65deffb87eb87bb4291c7c8ed1052b1279a355d3
LanaxCode <lana.supercode@gmail.com>
2024-02-12 20:53:20 +0100

    clean

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,3 @@
-// import { useState} from "react";
-// import '../css/SearchBar.css'
-// import lupe from '../assets/img/lupe-50.png'
-
-// const SearchBar = ({ onSearch }) => {
-
-//     const [query, setQuery] = useState("");
-
-//     const handleInputChange = (e) => {
-//       setQuery(e.target.value);
-//     };
-
-//     const handleSearch = () => {
-//               onSearch(query);
-//       };
-
-//     return ( 
-
-// <div className="wrap">
-//     <div className="search">
-//       <input type="text" className="searchTerm" id="input_text" value={query} onChange={handleInputChange} placeholder="Search the Collection"></input>
-//       <button type="submit" className="searchButton" onClick={handleSearch}>
-//         <img className="icon" src={lupe}/>
-//       </button>
-//     </div>
-//   </div>
-
-
-//      );
-// }
- 
-// export default SearchBar;
 
 import { useState } from 'react';
 import '../css/SearchBar.css';
